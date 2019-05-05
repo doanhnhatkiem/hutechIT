05736f2a5d6b21f5def233ef0b121bcd7bf6c0eb
doanhnhatkiem <doanhnhatkiem@users.noreply.github.com>
2019-05-05 16:47:49 +0700

    Updates

---
+++ precache-manifest.b5e0bf6575bc3197e8db2e9a707f7b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88780c9946f0895c0a516e8c249fd4ba",
+    "url": "/hutechIT/index.html"
+  },
+  {
+    "revision": "cc00003867b89b629f86",
+    "url": "/hutechIT/static/css/main.fd3270c8.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/hutechIT/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "cc00003867b89b629f86",
+    "url": "/hutechIT/static/js/main.886e22a6.chunk.js"
+  },
+  {
+    "revision": "677dfe98f8fc72abb56c",
+    "url": "/hutechIT/static/js/runtime~main.84b1624e.js"
+  }
+]);
